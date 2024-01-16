6c450c7c7c908de3036eb50831a1341e446a9ce5
Kama <yuanrong234001@gmail.com>
2024-01-16 13:24:46 +0800

    [updated] PWA

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,40 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [
+		react(),
+		VitePWA({
+			strategies: "generateSW", // Default
+			includeAssets: ["favicon.ico", "Logo.png"],
+			registerType: "autoUpdate",
+			manifest: {
+				name: "ZustandTest",
+				short_name: "ZT",
+				description: ":D",
+				start_url: "./",
+				icons: [
+					{
+						src: "favicon.ico",
+						sizes: "64x64 32x32 24x24 16x16",
+						type: "image/x-icon",
+					},
+					{
+						src: "Logo.png",
+						type: "image/png",
+						sizes: "192x192",
+					},
+					{
+						src: "Logo.png",
+						type: "image/png",
+						sizes: "512x512",
+					},
+				],
+				display: "standalone",
+				theme_color: "#000000",
+				background_color: "#ffffff",
+			},
+		}),
+	],
+});
